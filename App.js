ffd51b9e0572c94acf4be9030bc7883539c03b63
Jollen Chen <jollen@moko365.com>
2018-04-23 09:09:05 +0800

    create splash screen

--- App.js
+++ App.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import Root from './src/native/index';
 import configureStore from './src/store/index';
+import { AppLoading } from 'expo';
 
 const { persistor, store } = configureStore();
 
+/*
 export default function App() {
 	return <Root store={store} persistor={persistor} />;
+}*/
+
+export default class App extends React.Component {
+	constructor(props, context) {
+	    super(props, context);
+
+		this.state = {
+			isReady: false,
+		};    
+	}
+
+	preloading() {
+
+	}
+
+	render() {
+		if (!this.state.isReady) {
+			return (
+		        <AppLoading
+		          startAsync={this.preloading}
+		          onFinish={() => this.setState({ isReady: true })}
+		        />		
+			);
+		} else {
+			return <Root store={store} persistor={persistor} />;
+		}
+	}
 }
